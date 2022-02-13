63069d793541ccd0ee083cc5308dd72e9c99eb4c
Szymon Szaminko <sz.szaminko@gmail.com>
2022-02-13 11:48:41 +0100

    Add css styles and make first operations

--- firstLook/src/app/app.component.ts
+++ firstLook/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstLook';
+  title: string = 'Hello World! First look for Angular';
 }
