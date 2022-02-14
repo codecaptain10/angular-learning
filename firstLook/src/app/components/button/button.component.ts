52cc62d18e993fb8d291b0986d470aa3121a301d
Szymon Szaminko <sz.szaminko@gmail.com>
2022-02-14 03:06:54 +0100

    Create and add an event

--- firstLook/src/app/components/button/button.component.ts
+++ firstLook/src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ButtonComponent implements OnInit {
   @Input() color: string = '';
   @Input() text: string = '';
+  @Output() btnClick = new EventEmitter();
 
   constructor() {}
 
@@ -21,5 +22,7 @@ export class ButtonComponent implements OnInit {
     } else {
       this.color = 'green';
     }
+
+    this.btnClick.emit();
   }
 }
