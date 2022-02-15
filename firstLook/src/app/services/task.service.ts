f01fb174056acb423837b1af8c4cdd4b8aa508a9
Szymon Szaminko <sz.szaminko@gmail.com>
2022-02-15 12:36:08 +0100

    Add JSON server, create a fake API data and import http module

--- firstLook/src/app/services/task.service.ts
+++ firstLook/src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Task } from '../Task';
 import { TASKS } from '../mock-tasks';
 import { Observable, of } from 'rxjs';
